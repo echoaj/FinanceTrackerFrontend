21ce662eaee83ab05be82447e3710bb6446d9151
echoaj <aljoslin13@yahoo.com>
2022-01-12 12:35:44 -0800

    Fixed update

--- src/UpdateField.js
+++ src/UpdateField.js
@@ -14,9 +14,9 @@ class UpdateField extends Component {
 
     // Put expense when the form is submited
     async handlePut(event) {
+        event.preventDefault();
         await this.props.updateExpense(this.state.data);
         this.setState({ data: { "item": "" } });
-        event.preventDefault();
     }
 
     // Update state when the user types in the expense item
@@ -29,7 +29,7 @@ class UpdateField extends Component {
         const valueItem = this.state.data.item;
         const isEmpty = valueItem === "";
         return (
-            <form className="updateField" onSubmit={this.handlePut}>
+            <form className="updateField" onSubmit={this.handlePut} action="#">
                 <Form.Control className="updateFieldForm" type="text" placeholder="Update Item" value={valueItem} onChange={this.handleChange} />
                 <Button className="updateFieldButton" type="submit" variant="primary" disabled={isEmpty}>Update</Button>
                 <Button className="closeUpdateFieldButton" variant="danger" onClick={() => this.props.changeFieldTo("post")}>Cancel</Button>
